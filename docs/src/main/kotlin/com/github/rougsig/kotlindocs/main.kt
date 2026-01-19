a3e9744163a086aebfc9ffa452aa9c180c1cba90
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2026-01-19 21:32:06 +0100

    chore(docs): upgrade server deps and runtime

--- docs/src/main/kotlin/com/github/rougsig/kotlindocs/main.kt
+++ docs/src/main/kotlin/com/github/rougsig/kotlindocs/main.kt
@@ -1,11 +1,11 @@
 package com.github.rougsig.kotlindocs
 
-import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
