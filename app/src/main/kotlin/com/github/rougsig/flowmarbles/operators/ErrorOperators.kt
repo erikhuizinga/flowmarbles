855c0ebdbe314cf3ee59aaf2d18d90b8ee0b0b6d
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2026-01-29 17:09:42 +0100

    feat(ops): add retryWhen

--- app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/ErrorOperators.kt
+++ app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/ErrorOperators.kt
@@ -1,11 +1,13 @@
 package com.github.rougsig.flowmarbles.operators
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.retryWhen
 
 @ExperimentalCoroutinesApi
 fun errorOperators() = listOf(
@@ -54,6 +56,37 @@ fun errorOperators() = listOf(
           emit(value)
         }
       }.retry(1)
+    }
+  ),
+  menuItem(
+    label("retryWhen"),
+    sandbox(
+      "retryWhen",
+      inputs(
+        input(
+          marble("A", 0),
+          marble("B", 150),
+          marble("X", 300),
+          marble("C", 450)
+        )
+      ),
+      "retryWhen { delay(200); attempt < 1 }"
+    ) { inputs ->
+      var attempt = 0
+      flow {
+        attempt += 1
+        inputs[0].collect { value ->
+          if (value.value == "X" && attempt == 1) error("boom")
+          emit(value)
+        }
+      }.retryWhen { _, retryAttempt ->
+        if (retryAttempt == 0L) {
+          delay(200)
+          true
+        } else {
+          false
+        }
+      }
     }
   )
 )
