d3751c554f3bc40b20420fcf78945a1c9a787a90
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2026-01-29 17:02:28 +0100

    feat(ops): add distinctUntilChangedBy

--- app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/DistinctOperators.kt
+++ app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/DistinctOperators.kt
@@ -3,6 +3,7 @@ package com.github.rougsig.flowmarbles.operators
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.distinctUntilChangedBy
 
 @FlowPreview
 @ExperimentalCoroutinesApi
@@ -25,5 +26,25 @@ fun distinctOperators() = listOf(
       ),
       "distinctUntilChanged()"
     ) { inputs -> inputs[0].distinctUntilChanged { old, new -> old.value == new.value } }
+  ),
+  menuItem(
+    label("distinctUntilChangedBy"),
+    sandbox(
+      "distinctUntilChangedBy",
+      inputs(
+        input(
+          marble("A1", 0, color = Colors.colors[0]),
+          marble("A2", 150, color = Colors.colors[0]),
+          marble("B1", 300, color = Colors.colors[1]),
+          marble("B2", 450, color = Colors.colors[1]),
+          marble("B3", 600, color = Colors.colors[1]),
+          marble("C1", 750, color = Colors.colors[2]),
+          marble("D1", 900, color = Colors.colors[3])
+        )
+      ),
+      "distinctUntilChangedBy { it.value.first() }"
+    ) { inputs ->
+      inputs[0].distinctUntilChangedBy { it.value.first() }
+    }
   )
 )
