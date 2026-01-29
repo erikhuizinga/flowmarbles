36e9553f7cc2bdb4a0e1b23a6357de55c90acc73
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2026-01-29 17:05:05 +0100

    feat(ops): add all

--- app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/TerminalOperators.kt
+++ app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/TerminalOperators.kt
@@ -1,6 +1,7 @@
 package com.github.rougsig.flowmarbles.operators
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.all
 import kotlinx.coroutines.flow.any
 import kotlinx.coroutines.flow.flow
 
@@ -25,6 +26,27 @@ fun terminalOperators() = listOf(
       flow {
         val hasMatch = inputs[0].any { it.value > 4 }
         val output = if (hasMatch) "T" else "F"
+        emit(marble(output, 0, Colors.accentColors[0]))
+      }
+    }
+  ),
+  menuItem(
+    label("all"),
+    sandbox(
+      "all",
+      inputs(
+        input(
+          marble(1, 0),
+          marble(2, 150),
+          marble(3, 300),
+          marble(5, 600)
+        )
+      ),
+      "all { it < 5 }"
+    ) { inputs ->
+      flow {
+        val allMatch = inputs[0].all { it.value < 5 }
+        val output = if (allMatch) "T" else "F"
         emit(marble(output, 0, Colors.accentColors[0]))
       }
     }
