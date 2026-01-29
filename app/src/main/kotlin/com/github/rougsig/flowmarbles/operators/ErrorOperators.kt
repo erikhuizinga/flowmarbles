ac48931a6dcec6570f2cdf38d0e4bf127dcffe0f
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2026-01-29 17:09:16 +0100

    feat(ops): add retry

--- app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/ErrorOperators.kt
+++ app/src/main/kotlin/com/github/rougsig/flowmarbles/operators/ErrorOperators.kt
@@ -2,7 +2,10 @@ package com.github.rougsig.flowmarbles.operators
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.retry
 
 @ExperimentalCoroutinesApi
 fun errorOperators() = listOf(
@@ -27,6 +30,30 @@ fun errorOperators() = listOf(
           value
         }
         .catch { emit(marble("E", 0, Colors.accentColors[0])) }
+    }
+  ),
+  menuItem(
+    label("retry"),
+    sandbox(
+      "retry",
+      inputs(
+        input(
+          marble("A", 0),
+          marble("B", 150),
+          marble("X", 300),
+          marble("C", 450)
+        )
+      ),
+      "retry(1)"
+    ) { inputs ->
+      var attempt = 0
+      flow {
+        attempt += 1
+        inputs[0].collect { value ->
+          if (value.value == "X" && attempt == 1) error("boom")
+          emit(value)
+        }
+      }.retry(1)
     }
   )
 )
